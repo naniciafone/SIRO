0ba7c953181e840659fbb37dd9d3bc0f775745ab
naniciafone <nciafone@pdx.edu>
2026-01-10 20:37:57 -0700

    updates

--- scripts/LiDAR_resample.py
+++ scripts/LiDAR_resample.py
@@ -32,8 +32,15 @@ for src_path in src_dir.glob("*.tif"):
     # Trim edges to be divisible by scale
     # -----------------------
     h, w = data.shape
+    # h = height (# of rows), w = width (number of columns) of reference raster
+    # determine how many rows/ columns to keep when rescaling, where h2 and w2 are width and height of output raster
+    # i.e., make width and height of output dataset multiple of scale (200)
     h2 = h - (h % scale)
+    # 'h % scale' returns the remainder when h is divided by scale
+    # could also use, h2 = (h // scale) * scale
     w2 = w - (w % scale)
+    # 'w % scale' returns the remainder when w is divided by scale
+    # could also use, w2 = (w // scale) * scale
     data = data[:h2, :w2]
 
     # -----------------------
@@ -43,17 +50,27 @@ for src_path in src_dir.glob("*.tif"):
         h2 // scale, scale,
         w2 // scale, scale
     )
-
+    # reshape takes a 2D array (h2, w2) and splits it into 4 dimensions:
+        # blocks.shape == (coarse_rows, fine_rows, coarse_cols, fine_cols)
+    # where course is the size of the resampled pixel (100-m), and fine is the original pixels that will be used to compute the value of the block
+    # reshape does no copying, just groups pixels into blocks, ready for aggregation.
     # -----------------------
+
     # Aggregate
     # -----------------------
     mean = np.nanmean(blocks, axis=(1, 3))
+    # axis relative to blocks.shape (coarse_rows, fine_rows, coarse_cols, fine_cols)
+    # so, averaging across pixels in the block
 
     coverage = (
         np.isfinite(blocks).sum(axis=(1, 3))
+    # returns a boolean array of the same shape, True is pixel is a number, false if pixel is NAN
+    # so, ensures the sum of the fine rows is not 0
+    #determines how many of the pixels (percentage) in a block are NAN
         / (scale * scale)
     )
 
+    #if coverage is less than 10%, make block NAN
     mean[coverage < min_coverage] = np.nan
 
     # -----------------------
